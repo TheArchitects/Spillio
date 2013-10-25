879a1cd547f673c1af0a26bcabdf0f675df0d2a0
kayvannj <kayvan.najafzadeh@gmail.com>
2013-10-24 18:34:19 -0700

    UCB Login Installed

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,28 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20131025010602) do
+
+  create_table "announcements", :force => true do |t|
+    t.text     "message"
+    t.datetime "starts_at"
+    t.datetime "ends_at"
+    t.boolean  "active"
+    t.text     "roles"
+    t.text     "types"
+    t.text     "style"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "hidden_announcements", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "announcement_id"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+  end
+
+  add_index "hidden_announcements", ["announcement_id"], :name => "index_hidden_announcements_on_announcement_id"
+  add_index "hidden_announcements", ["user_id"], :name => "index_hidden_announcements_on_user_id"
 
 end
