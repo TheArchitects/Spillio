9e6888f233eb101f53be9c3127a0851b0e79c8a9
Kayvan <kayvan.najafzadeh@gmail.com>
2013-10-27 22:33:39 -0700

    facebook login

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+	def create
+		auth = request.env["omniauth.auth"]
+		user = FbUser.where(:provider => auth['provider'], 
+		                :uid => auth['uid']).first || FbUser.create_with_omniauth(auth)
+		session[:user_id] = user.id
+
+		#redirect to user profile after successful signin
+		redirect_to root_url, :notice => "Signed in!"
+	end
+
+	def new
+		redirect_to '/auth/facebook'
+	end
+
+	def destroy
+		reset_session
+		#redirect to homepage
+		redirect_to root_url, notice => 'Signed out'
+	end
+
+	def failure
+		flash[:notice] = "Login Failed, please try again."
+		redirect_to login_path
+	end
+end
