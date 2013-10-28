b24bb8fc5b1b635f341ec22835b9009ed73d30f1
Kayvan <kayvan.najafzadeh@gmail.com>
2013-10-28 00:29:19 -0700

    changing fb key

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,10 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, "1424803487747929", "01eb2f03a96d50dd920cb8fd6a2308cd",
-           :scope => 'email,user_birthday,read_stream', :display => 'popup'
+  #provider :facebook, "1424803487747929", "01eb2f03a96d50dd920cb8fd6a2308cd",
+  #        :scope => 'email,user_birthday,read_stream', :display => 'popup'
+
+	provider :facebook, "571524266252772", "395b853d3d95a2e65540ece5babb04a2",
+    	:scope => 'email,user_birthday,read_stream', :display => 'popup'
+
 end
 
 OmniAuth.config.on_failure = Proc.new do |env| new_path = "/auth/failure"
