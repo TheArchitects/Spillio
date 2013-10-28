dd3059aa90dc0110ef12eb73ba97d91f1044f1d2
Jalal Buckley <greenmintleaf@gmail.com>
2013-10-27 23:26:09 -0700

    students_controller_test

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,18 @@
 
 ActiveRecord::Schema.define(:version => 20131027215357) do
 
+  create_table "announcements", :force => true do |t|
+    t.text     "message"
+    t.datetime "starts_at"
+    t.datetime "ends_at"
+    t.boolean  "active"
+    t.text     "roles"
+    t.text     "types"
+    t.text     "style"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
   create_table "courses", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
@@ -27,6 +39,25 @@ ActiveRecord::Schema.define(:version => 20131027215357) do
   end
 
   add_index "courses_students", ["course_id", "student_id"], :name => "index_courses_students_on_course_id_and_student_id", :unique => true
+
+  create_table "fb_users", :force => true do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "hidden_announcements", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "announcement_id"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+  end
+
+  add_index "hidden_announcements", ["announcement_id"], :name => "index_hidden_announcements_on_announcement_id"
+  add_index "hidden_announcements", ["user_id"], :name => "index_hidden_announcements_on_user_id"
 
   create_table "sections", :force => true do |t|
     t.integer  "number"
@@ -61,5 +92,26 @@ ActiveRecord::Schema.define(:version => 20131027215357) do
   end
 
   add_index "students", ["section_id"], :name => "index_students_on_section_id"
+
+  create_table "users", :force => true do |t|
+    t.string   "uid",             :limit => 10,                     :null => false
+    t.string   "first_name",      :limit => 60
+    t.string   "last_name",       :limit => 60
+    t.string   "first_last_name", :limit => 100
+    t.string   "email",           :limit => 256
+    t.string   "phone",           :limit => 30
+    t.boolean  "inactive",                       :default => false, :null => false
+    t.boolean  "admin",                          :default => false, :null => false
+    t.datetime "last_login_at"
+    t.datetime "last_request_at"
+    t.datetime "last_logout_at"
+    t.datetime "created_at",                                        :null => false
+    t.datetime "updated_at",                                        :null => false
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email"
+  add_index "users", ["first_name"], :name => "index_users_on_first_name"
+  add_index "users", ["last_name"], :name => "index_users_on_last_name"
+  add_index "users", ["uid"], :name => "index_users_on_uid", :unique => true
 
 end
