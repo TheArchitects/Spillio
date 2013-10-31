0c4c1d8db47f0724b7fae82992b62fb4794381bf
Alfonso GJ <alfongj@gmail.com>
2013-10-31 10:21:41 -0700

    Fix for assets not to break heroku deploy

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Spillio
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Necessary for Heroku to work
+    config.assets.initialize_on_precompile = false;
   end
 end
