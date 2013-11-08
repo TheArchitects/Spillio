9c4a84fbc5e7ccc9f73b1b1f5323d95ec46413bb
Alfonso GJ <alfongj@gmail.com>
2013-11-07 23:54:51 -0800

    Removed notifications, not sure if they were compatible with everything else

--- app/models/notification.rb
+++
@@ -1,5 +0,0 @@
-class Notification < ActiveRecord::Base
-  belongs_to :from, class_name: "Student"
-  belongs_to :to, class_name: "Student"
-  attr_accessible :response, :sender_notified, :timestamps, :type, :from, :to
-end
