9b30297a9fb81dd1089c3e06ee75327be70c8fdc
Arturo Pacifico Griffini <pacifico.arturo@gmail.com>
2013-11-07 21:38:54 -0800

    resolved conflicts

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,9 +1,5 @@
 class Section < ActiveRecord::Base
   has_many :students
-<<<<<<< HEAD
   has_many :groups
-=======
-  belongs_to :course
->>>>>>> f54d386e83e4db1b209a9f8db45b1c177b733f6e
   attr_accessible :number
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,26 +1,5 @@
-<<<<<<< HEAD
+
 class Student < User
   belongs_to :group
   # attr_accessible :title, :body
-=======
-class Student < ActiveRecord::Base
-  belongs_to :group :section
-  has_and_belongs_to_many :courses :skills 
-  has_many :notifications :posts :comments
-
-  attr_accessible :about, :interest, :name, :cid
-
-  def self.search_by_name(query, page)
-    # TODO: Avoid SQL injection!!
-    query = query.downcase
-    sql_query = Student.where("lower(name) like ?", "%#{query}%")
-    return Student.num_pages_from_query(sql_query), sql_query.paginate(:per_page => 20, :page => page)
-  end
-
-  private
-  def self.num_pages_from_query(query)
-    total_res = query.count
-    1 + (total_res-1) / 20
-  end
->>>>>>> f54d386e83e4db1b209a9f8db45b1c177b733f6e
 end
