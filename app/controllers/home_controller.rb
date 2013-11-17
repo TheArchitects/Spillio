d86df8c0f21bf669a3cfde35750768eb0ec43600
Alfonso GJ <alfongj@gmail.com>
2013-11-16 18:46:26 -0800

    Fixed some things related to login that were doing incorrect redirects if the cache was dirty. Fixed scenario in which an user with no acct could access internal pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,12 +4,9 @@ class HomeController < ApplicationController
 	#before_filter CASClient::Frameworks::Rails::Filter, :except => :index
 
 	def index
-		if session[:cas_user]!=nil
-			#redirect_to user_account_path
+		if (not session[:cas_user].nil?) and User.exists_with_cid? session[:cas_user]
 			authenticated_user = User.find_by_cid(session[:cas_user])
-			unless authenticated_user.nil?
-				redirect_to group_db_show_url authenticated_user.group.id
-			end
+			redirect_to group_db_show_url authenticated_user.group.id
 		end
 
 		@login_link = login_path
