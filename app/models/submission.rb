b5e3cd25add8fcbbe23661a91e8d77f51a4c1434
Jalal Buckley <greenmintleaf@gmail.com>
2013-12-07 19:43:14 -0800

    So far progress on assign grade

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,4 +1,8 @@
 class Submission < ActiveRecord::Base
   belongs_to :assignment
   attr_accessible :label, :content, :submitted_date, :assignment_id
+
+  def max_grade
+    self.assignment.max_grade
+  end
 end
