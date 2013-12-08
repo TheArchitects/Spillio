94a2ea4dbedd8eb74df6b1259e086335e3490ad9
Jalal Buckley <greenmintleaf@gmail.com>
2013-12-08 04:41:30 -0800

    changed assign grades to use post instead of get http request

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,6 +1,6 @@
 class Submission < ActiveRecord::Base
   belongs_to :assignment
-  attr_accessible :label, :content, :submitted_date, :assignment_id
+  attr_accessible :label, :content, :submitted_date, :assignment_id, :subm_type
 
   def max_grade
     self.assignment.max_grade
