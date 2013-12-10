f8c00a48b16338dc9a4e39a63a117740edd17907
kayvan <kayvan.najafzadeh@gmail.com>
2013-12-10 00:52:47 -0800

    fixed a bug, reader breaks if there is a group with out a reader

--- app/controllers/reader_controller.rb
+++ app/controllers/reader_controller.rb
@@ -3,7 +3,7 @@ class ReaderController < AuthenticatedController
 	def my_groups
 		@any_results = true
 		@groups = Group.all.select{ |group|
-			group.reader.id == @authenticated_user.id
+			(not group.reader.nil?)	and (group.reader.id == @authenticated_user.id)
 		}
 		if @groups.empty?
 			@any_results = false
