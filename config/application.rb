79cfd973440b4f26007ee31221a4789ec0d805da
kayvan <kayvan.najafzadeh@gmail.com>
2013-12-10 02:18:50 -0800

    increasing asset version

--- config/application.rb
+++ config/application.rb
@@ -57,7 +57,7 @@ module Spillio
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.3'
+    config.assets.version = '1.4'
 
     # Necessary for Heroku to work
     config.assets.initialize_on_precompile = false;
